448d62af63e3d2713f88a2085e5a3a68d12f93ee
Matthew Wagaman <averaqedev@gmail.com>
2020-03-17 11:31:56 -0700

    prepare for deploy

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DB_URL || 'postgresql://ezpunishments@localhost/ezpunishments',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://ezpunishments@localhost/ezpunishments',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '300s',
   RESET_PASSWORD_EXPIRY: process.env.RESET_PASSWORD_EXPIRY || '300',
