209e8561e097595bd99cb17c73aed4cbbacd40e3
Matthew Wagaman <averaqedev@gmail.com>
2020-03-18 04:39:53 -0700

    added to config

--- src/config.js
+++ src/config.js
@@ -1,9 +1,12 @@
 module.exports = {
+  // Basic Info
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://ezpunishments@localhost/ezpunishments',
+  // JWT Info
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-  JWT_EXPIRY: process.env.JWT_EXPIRY || '300s',
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '3m',
+  // Password Reset Email OAuth
   RESET_PASSWORD_EXPIRY: process.env.RESET_PASSWORD_EXPIRY || '300',
   EMAIL_USER: process.env.EMAIL_USER,
   CLIENT_ID: process.env.CLIENT_ID,
